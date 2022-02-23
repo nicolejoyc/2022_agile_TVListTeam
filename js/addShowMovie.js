4fafad0b5f8afe3ff8d553f509e284c24f72a1f9
Nicole Crumbaker <ncrumbaker@student.cvtc.edu>
2022-02-22 19:50:06 -0600

    Edited pages to add show/movie to db

--- js/addShowMovie.js
+++ js/addShowMovie.js
@@ -1,31 +1,35 @@
-async function postData(action = '', data = {}) {
-  // Default options are marked with *
-  const response = await fetch("https://api.m3o.com/v1/db/" + action, {
-    method: 'POST', // *GET, POST, PUT, DELETE, etc.
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': '' // TODO : add public authorization key here
-      // 'Content-Type': 'application/x-www-form-urlencoded',
-    },
-    body: JSON.stringify(data) // body data type must match "Content-Type" header
-  });
-  return response.json(); // parses JSON response into native JavaScript objects
-}
+var addEntry = new DBCreateTransaction();
 
 document.querySelector("#addShowMovieForm").addEventListener("submit", function(e){
-
   // For now, added this so that the page doesn't refresh
   // but will probably want to refresh to another page later
   // to display the info that the user just submitted
   e.preventDefault(); 
 
-  // Calls the postData function with the "create" action which allows to 
-  // add a record to the database
-  postData("Create", { record: {
-    // TODO add record info
-  }, table: '' })   // TODO: add table name
-  .then(data => {
-    console.log(data); // JSON data parsed by `data.json()` call
-  });
-});
+  var inputTitle = document.querySelector("#title");
+  var inputTvType = document.querySelector('input[name="tvType"]:checked');
+  var inputGenre = document.querySelector("#genre");
+  var inputLength = document.querySelector("#length");
+  var inputWishList = document.querySelector("#wishlist");
+  //console.log(inputTitle.value + "\n" + inputTvType.value + "\n" +
+  //              inputGenre.value + "\n" + inputLength.value + "\n" +
+  //              inputWishList.checked);
 
+  // TODO: add code to validate that user added at least title
+  //       and whether it's a show or movie
+
+  // TODO: add if statement to check if movie/show 
+  //       table has already been created, and if not create it??
+
+  // Calls the sendRequest method from addEntry instance of the
+  // DBCreateTransaction Class
+  addEntry.sendRequest("tv", {
+    id: "3", // TODO: add count method that autimatically adds another id
+    title: inputTitle.value,
+    tvType: inputTvType.value,
+    genre: inputGenre.options[inputGenre.selectedIndex].text,
+    length: inputLength.value,
+    wishList: inputWishList.checked
+  })
+
+});
