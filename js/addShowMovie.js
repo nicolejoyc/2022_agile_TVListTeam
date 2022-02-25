bacc9f19625e423fe1b1de8667f7d31d94ae040f
Nicole Crumbaker <ncrumbaker@student.cvtc.edu>
2022-02-24 20:02:18 -0600

    Added validation features to addShowMovie files

--- js/addShowMovie.js
+++ js/addShowMovie.js
@@ -1,28 +1,7 @@
-m3oKey = "";
+m3oKey = "NzZjMGM4ZjYtYmUyOC00NTFmLWIzYjEtNjY2NjEyMjIwYmRl";
 
-var createTransactor = new DBCreateTransaction();
-
-// Event listener for detecting when the genre selection is changed
-// this way, I can display the "other genre" textbox if their selection
-// is "Other"
-document.querySelector("#genre").addEventListener("change", function(e) {
-
-  if (this.options[this.selectedIndex].text === "Other") {
-    document.querySelector("#otherTxt").style.display = "inline";
-    document.querySelector("#otherLbl").style.display = "inline";
-  } else {
-    document.querySelector("#otherTxt").style.display = "none";
-    document.querySelector("#otherLbl").style.display = "none";
-  }
-
-})
-
-// Event listener for the form submit
-document.querySelector("#addShowMovieForm").addEventListener("submit", function(e){
-  // For now, added this so that the page doesn't refresh
-  // but will probably want to refresh to another page later
-  // to display the info that the user just submitted
-  e.preventDefault(); 
+function addShowMovie(recordID) {
+  var createTransactor = new DBCreateTransaction();
 
   var inputTitle = document.querySelector("#title");
   var inputShowOrMovie = document.querySelector('input[name="showOrMovie"]:checked');
@@ -37,31 +16,110 @@ document.querySelector("#addShowMovieForm").addEventListener("submit", function(
   var inputLength = document.querySelector("#length");
   var inputViewed = document.querySelector("#viewed");
   var inputWishList = document.querySelector("#wishlist");
-  //console.log(inputTitle.value + "\n" + inputshowOrMovie.value + "\n" +
-  //              inputGenre.value + "\n" + inputLength.value + "\n" +
-  //              inputWishList.checked);
 
-  // TODO: add code to validate that user added at least title
-  //       and whether it's a show or movie
-
-  // TODO: check if the "other" option is selected
-  //       if it's selected, inputGenre should be the input from 
-  //       the "Other" text box, otherwise inputGenre should be 
-  //       the value from the drop down list 
+  // check if the "other" option is selected
+  // if it's selected, inputGenre should be the input from 
+  // the "Other" text box, otherwise inputGenre should be 
+  // the value from the drop down list 
   if (inputSelectedGenre.options[inputSelectedGenre.selectedIndex].text === "Other") {
     inputGenre = inputOtherGenre.value;
   } else {
     inputGenre = inputSelectedGenre.options[inputSelectedGenre.selectedIndex].text;
   }
 
+  // Calls the sendRequest method from createTransactor instance of the
+  // DBCreateTransaction Class
+  createTransactor.sendRequest("tvlist", {
+    id: recordID,
+    title: inputTitle.value,
+    showOrMovie: inputShowOrMovie.value,
+    director: inputDirector.value,
+    releaseYear: inputReleaseYear.value,
+    genre: inputGenre,
+    length: inputLength.value,
+    viewed: inputViewed.checked,
+    wishList: inputWishList.checked,
+    rating: getInputRating()
+  });
+
+} // end addShowMovie function
+
+// Function that returns the correct rating
+// prevents adding a rating to a movie that has not been viewed
+function getInputRating() {
+  if (document.querySelector("#viewed").checked) {
+    return document.querySelector("#rating").value;
+  } else {
+    return "";
+  }
+}
+
+// Event listener for detecting when the genre selection is changed
+// this way, I can display the "other genre" textbox if their selection
+// is "Other"
+document.querySelector("#genre").addEventListener("change", function(e) {
+
+  if (this.options[this.selectedIndex].text === "Other") {
+    document.querySelector("#otherTxt").style.display = "inline";
+    document.querySelector("#otherLbl").style.display = "inline";
+  } else {
+    document.querySelector("#otherTxt").style.display = "none";
+    document.querySelector("#otherLbl").style.display = "none";
+  }
+
+});
+
+// Event listener for detecting if the user checks the "viewed" box
+// if so, the rating div should appear
+document.querySelector("#viewed").addEventListener("change", function(e) {
+  if (this.checked) {
+    document.querySelector("#ratingDiv").style.display = "block";
+  } else {
+    document.querySelector("#ratingDiv").style.display = "none";
+  }
+
+});
+
+// Set paragraph next to rating to 0, then add event listener
+// to detect when value changes and update paragraph with new value
+var currentRating = document.querySelector("#currentRating");
+var ratingSlider = document.querySelector("#rating");
+currentRating.innerHTML = ratingSlider.value;
+
+ratingSlider.addEventListener("input", function(e) {
+  currentRating.innerHTML = ratingSlider.value;
+});
+
+
+// Event listener for the form submit
+document.querySelector("#addShowMovieForm").addEventListener("submit", function(e){
+  // For now, added this so that the page doesn't refresh
+  // but will probably want to refresh to another page later
+  // to display the info that the user just submitted
+  e.preventDefault(); 
+
+  // validate that user added at least title
+  // and whether it's a show or movie
+  function validateInput() {
+    if (document.querySelector("#title").value === "") {
+      alert("Please enter a title.")
+      return false;
+    } else if (document.querySelector('input[name="showOrMovie"]:checked') === null) {
+      alert("Please specify whether your entry is a show or movie.")
+      return false;
+    } else {
+      return true;
+    }
+  }
+
   // check if movie/show table has already been created
   // if created => determine next id
   // if not created => set id of the first record to one more than the
   // greatest id
-  var newRecordId = "";
 
   // Response handler to manipulate the returned info
   listTablesRspHandler = (obj) => {
+    var newRecordID = "";
     tables = obj['tables'];
 
     function tblExists(table) {
@@ -74,14 +132,16 @@ document.querySelector("#addShowMovieForm").addEventListener("submit", function(
       queryRspHandler = (obj) => {
         records = obj['records'];
 
-        // sets the newRecordId to one plus the id of the current
+        // sets the newRecordID to one plus the id of the current
         // record with the highest id
         // this way, even if a record between the lowest and highest
         // id is deleted, the next stored id will still be accurate
-        newRecordId = Number(records[0].id) + 1;
+        newRecordID = (Number(records[0].id) + 1).toString();
+
+        addShowMovie(newRecordID);
       };
 
-      var queryTransactor = new DBQueryTransaction(null, queryRspHandler);
+      var queryTransactor = new DBQueryTransaction(queryRspHandler);
 
       // sends a request to query the tvList data to only return the
       // record with the highest id (id desc)
@@ -94,25 +154,18 @@ document.querySelector("#addShowMovieForm").addEventListener("submit", function(
 
     } else {
       // table does not exist, so set first record to id of 1
-      newRecordId = "1";
-    };
+      newRecordID = "1";
+
+      // Calls the sendRequest method from createTransactor instance of the
+      // DBCreateTransaction Class
+      addShowMovie(newRecordID);
+    }
   };
 
-  var listTablesTransactor = new DBListTablesTransaction(null, listTablesRspHandler);
+  var listTablesTransactor = new DBListTablesTransaction(listTablesRspHandler);
 
-  listTablesTransactor.sendRequest();
+  if (validateInput()) {
+    listTablesTransactor.sendRequest();
+  }
 
-  // Calls the sendRequest method from createTransactor instance of the
-  // DBCreateTransaction Class
-  createTransactor.sendRequest("tvlist", {
-    id: newRecordId, // TODO: adds record, but is currently not setting newRecordId above
-    title: inputTitle.value,
-    showOrMovie: inputShowOrMovie.value,
-    director: inputDirector.value,
-    releaseYear: inputReleaseYear.value,
-    genre: inputGenre,
-    length: inputLength.value,
-    viewed: inputViewed.checked,
-    wishList: inputWishList.checked
-  })
 });
