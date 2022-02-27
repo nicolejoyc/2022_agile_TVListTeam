90f0ede4fd4b4d6fd56115e4d3d1a234c0853afe
Richard Valien <98352633+rvalien-student@users.noreply.github.com>
2022-02-27 07:48:05 -0600

    [finishes #181333398] Addition of user login functionality, including the login page, styling, behavior and error checking / handling.

---
+++ js/common.js
@@ -0,0 +1,27 @@
+/**
+ * Application Shared Variables
+ */
+signInStorageKey = 'signIn';
+userAccountTableName = 'login';
+
+/**
+ * M3O Cloud Key Update
+ * 
+ * Provide a series of pop-up dialogs to allow user to
+ * update their M3O Cloud Services access key.
+ */
+updateM3oKey = () => {
+  // Get M3O Key from user
+  if(!(m3oKey = window.prompt("Please Enter M3OKey:", ""))) {
+    return;
+  }
+  m3oKeyObj.reset();
+  // Rember key on this computer, save local
+  if(window.confirm("Remember key on this computer?")) {
+    m3oKeyObj.setLocalKey(m3oKey);
+  }
+  // Do not remember key, save session
+  else {
+    m3oKeyObj.setSessionKey(m3oKey);
+  }
+};
--- js/tvList.js
+++ js/tvList.js
@@ -1,71 +1,11 @@
 $(function() {
 
-  // Global key needed for M3O cloud service access
-  if(m3oKeyObj.isKeyUndefined()) {
-    // Get M3O Key from user
-    while(!(m3oKey = window.prompt("Please Enter M3OKey:", ""))) {
-      // Continue until key is entered
-    }
-    // Rember ky on this computer, save key local
-    if(window.confirm("Remember key on this computer?")) {
-      m3oKeyObj.setLocalKey(m3oKey);
-    }
-    // Do not remember key, save key for session
-    else {
-      m3oKeyObj.setSessionKey(m3oKey);
-    }
-  }
-
   /**
-   * The following is code to demonstrate use of
-   * database cloud transactions chained together:
-   * 
-   *  create record -> read record -> delete record
-   * 
-   * The result of each transaction is sent to the console
-   * from the transaction response handlers.
-   * 
-   * Reload web page to see execute code.
-   * 
-   * NOTE: M3O AUTHORIZATION KEY IS REQUIRED ABOVE (m3oKey)
-   * 
-   * REMOVE CODE WHEN AN IMPLEMENTATION IS COMPLETE
+   * Redirect to login page when user is not signed in.
    */
-
-  /*
-      // Table record 'id'
-      let id = "10";
-      let tableName = 'people';
-
-      // Database delete response handler
-      deleteRspHandler = (obj) => {
-        console.log(JSON.stringify(obj));
-        // Delete done, chain complete
-      };
-      
-      // Database query response handler
-      queryRspHandler = (obj) => {
-        console.log(JSON.stringify(obj));
-        // Database delete transaction
-        let deleteTransactor = new DBDeleteTransaction(deleteRspHandler);
-        // Send database delete request
-        deleteTransactor.sendRequest(tableName, id);
-      };
-
-      // Database create response handler
-      createRspHandler = (obj) => {
-        console.log(JSON.stringify(obj));
-        // Database query transaction
-        let queryTransactor = new DBQueryTransaction(queryRspHandler);
-        // Send database query request
-        queryTransactor.sendRequest(tableName, { "query": "name == 'John' && age == 35" });
-      };
-      
-      // Database create transaction
-      let createTransactor = new DBCreateTransaction(createRspHandler);
-      // Send database create request (create table if doesn't exist)
-      createTransactor.sendRequest(tableName, { 'id': id, 'name': 'John', 'age': 35, 'member': true });
- */
+  if(!(sessionStorage.getItem(signInStorageKey)) && !(localStorage.getItem(signInStorageKey))) {
+    location.assign('login.html');
+  }
   
 });
 
