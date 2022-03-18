74542a4b141aef0bfb0a4c30063860c57be1a0b0
Richard Valien <98352633+rvalien-student@users.noreply.github.com>
2022-03-18 11:08:32 -0500

    Moved code from accountList.js to accountView.js.

--- js/accountView.js
+++ js/accountView.js
@@ -136,3 +136,40 @@ var queryAccountRspHandler = (obj) => {
   } 
 
 };
+
+$(function() {
+
+  /**
+   * Redirect to login page when user is not signed in.
+   */
+  if(!isSignedIn()) {
+    location.assign('login.html');
+  }
+  /**
+   * User is signed in, proceed!
+   */
+  else {
+    // Instantiate the header drop-down menu
+    var dropDownMenu = new DropDownMenu($('#dd-menu'));
+    // Update drop-down title to user email address
+    $('#dd-menu span').get(0).innerHTML = getSignedInKey();
+
+    // Collapse drop-down menu on document click
+    $(document).click(function() {
+      $('.wrapper-dropdown').removeClass('active');
+    });
+
+    // Personalize header
+    updateHeaderUsername($('div.header-center h1'), "'s Account");
+
+    // Query for user account
+    var emailAddress;
+    var queryAccountTransactor = new DBQueryTransaction(queryAccountRspHandler);
+    if((emailAddress = getSignedInKey())) {
+      queryAccountTransactor.sendRequest(userAccountTableName, {
+        "query": `email == "${emailAddress}"`
+      });
+    }
+  }
+  
+});
